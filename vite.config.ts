ca29304ea25002ed4fb8c365670c2cd491c36938
e_lll <gabriela.balisacan01@gmail.com>
2024-11-07 01:54:02 +0800

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 
 const config: UserConfig = {
-	plugins: [UnoCSS(), sveltekit()]
+	plugins: [UnoCSS(), sveltekit()], 
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern-compiler', // or "modern"
+				silenceDeprecations: ["legacy-js-api"],  //temp fix, 
+			}
+		}
+	}
 };
 
+
 export default config;
+
+
+
