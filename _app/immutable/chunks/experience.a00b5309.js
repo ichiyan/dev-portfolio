0392521070b181e61aea10074a91d90e9e1d95db
ichiyan <74673566+ichiyan@users.noreply.github.com>
2024-11-28 16:55:37 +0000

    Deploying to gh-pages from @ ichiyan/dev-portfolio@d8e28db50252b81d87dd2c0d08152d7706dc1f09 🚀

---
+++ _app/immutable/chunks/experience.a00b5309.js
@@ -0,0 +1,10 @@
+import{A as e}from"./UIcon.49f91b2a.js";import{b as t}from"./skills.bd9ea3d4.js";import{C as o}from"./types.5d7d966d.js";const i=[{slug:"software-engineer",company:"Alliance Software Inc.",description:`<ul>
+						<li>Worked on the front and back-end development of a web-based supply chain management system and e-commerce system using ReactJS, Java Spring, and MySQL</li>
+						<li>Wrote SQL scripts for data fetching, merging, aggregating, and more to create reports</li>
+						<li>Streamlined development processes by creating reusable components that could be shared across different parts of the application</li>
+						<li>Optimized code performance by identifying bottlenecks and refactoring for improved efficiency</li>
+					<ul>`,contract:o.Internship,type:"Software Development",location:"Cebu, PH",period:{from:new Date(2023,4,29),to:new Date(2023,8,26)},skills:t("js","reactjs","java","spring","sql","mysql","dbeaver"),name:"Software Engineer",color:"red",links:[],logo:e.ASI,shortDescription:`Worked on the front and back-end development of a web-based supply chain management system and e-commerce system and 
+							wrote SQL scripts for data fetching, merging, aggregating, and more to create reports`},{slug:"software-developer",company:"Trent",description:`<ul>
+						<li>Implemented the front-end screens of a delivery service mobile application using Flutter</li>
+						<li>Used the Google Maps API to track delivery riders' location</li>
+					  </ul>`,contract:o.Subcontract,type:"Software Development",location:"Remote",period:{from:new Date(2021,9,11),to:new Date(2022,2,1)},skills:t("dart","flutter","google-maps","android-studio"),name:"Software Developer",color:"orange",links:[],logo:e.Unknown,shortDescription:"Developed mobile applications for the client's delivery services."}],s="Experience";export{i,s as t};
