6bc7c68d5ce655ba2697673596712db94b771322
ichiyan <74673566+ichiyan@users.noreply.github.com>
2025-02-04 14:06:03 +0000

    Deploying to gh-pages from @ ichiyan/dev-portfolio@38b620d9e0f833e7cabda463a8588875fe0a3b25 🚀

--- _app/immutable/chunks/experience.d5bde7a0.js
+++ _app/immutable/chunks/experience.d5bde7a0.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.d16af4b8.js";import{b as t}from"./skills.000c658c.js";import{C as o}from"./types.5d7d966d.js";const i=[{slug:"software-engineer",company:"Alliance Software Inc.",description:`<ul>
+import{A as e}from"./UIcon.27380a48.js";import{b as t}from"./skills.84cc83aa.js";import{C as o}from"./types.5d7d966d.js";const i=[{slug:"software-engineer",company:"Alliance Software Inc.",description:`<ul>
 						<li>Worked on the front and back-end development of a web-based supply chain management system and e-commerce system using ReactJS, Java Spring, and MySQL</li>
 						<li>Wrote SQL scripts for data fetching, merging, aggregating, and more to create reports</li>
 						<li>Streamlined development processes by creating reusable components that could be shared across different parts of the application</li>
@@ -7,4 +7,4 @@ import{A as e}from"./UIcon.d16af4b8.js";import{b as t}from"./skills.000c658c.js"
 							wrote SQL scripts for data fetching, merging, aggregating, and more to create reports`},{slug:"software-developer",company:"Trent",description:`<ul>
 						<li>Implemented the front-end screens of a delivery service mobile application using Flutter</li>
 						<li>Used the Google Maps API to track delivery riders' location</li>
-					  </ul>`,contract:o.Subcontract,type:"Software Development",location:"Remote",period:{from:new Date(2021,9,11),to:new Date(2022,2,1)},skills:t("dart","flutter","google-maps","android-studio"),name:"Software Developer",color:"orange",links:[],logo:e.Unknown,shortDescription:"Developed mobile applications for the client's delivery services."}],s="Experience";export{i,s as t};
+					  </ul>`,contract:o.Subcontract,type:"Software Development",location:"Remote",period:{from:new Date(2021,9,11),to:new Date(2022,2,1)},skills:t("dart","flutter","google-maps","android-studio"),name:"Software Developer",color:"orange",links:[],logo:e.Unknown,shortDescription:"Developed mobile applications for the client's delivery services"}],s="Experience";export{i,s as t};
